26ed7eba0f4f27c60490864d56f6f45ee5129b9b
Ivan Nekrasov <inekrasov@parseq.pro>
2024-09-16 19:45:49 +0400

    Add project structure

---
+++ src/main/kotlin/com/illarionova/vika/paymaster/PayMasterApplication.kt
@@ -0,0 +1,11 @@
+package com.illarionova.vika.paymaster
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PayMasterApplication
+
+fun main(args: Array<String>) {
+	runApplication<PayMasterApplication>(*args)
+}
---
+++ src/test/kotlin/com/illarionova/vika/paymaster/PayMasterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.illarionova.vika.paymaster
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PayMasterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
