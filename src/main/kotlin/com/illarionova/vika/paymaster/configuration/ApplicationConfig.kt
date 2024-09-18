42521322cebe8c32d9927bf39655f8ae88b91739
Ivan Nekrasov <ivanvinekasov@gmail.com>
2024-09-19 01:46:04 +0400

    Refactor ApplicationConfig and Update Database Configuration
        - Removed @EnableJpaRepositories annotation from ApplicationConfig.kt.
        - Updated the database URL in application.yaml to point to the db container instead of localhost.
        - Renamed the db.migration directory to db/migration for database migration files.

--- src/main/kotlin/com/illarionova/vika/paymaster/configuration/ApplicationConfig.kt
+++ src/main/kotlin/com/illarionova/vika/paymaster/configuration/ApplicationConfig.kt
@@ -5,7 +5,6 @@ import com.illarionova.vika.paymaster.service.CustomUserDetailsService
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.AuthenticationProvider
@@ -21,7 +20,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 @EnableWebMvc
 @EnableConfigurationProperties(JwtProperties::class)
-@EnableJpaRepositories(basePackages = ["com.illarionova.vika.paymaster.repository"])
 @EnableScheduling
 class ApplicationConfig : WebMvcConfigurer {
 
